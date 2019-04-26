552b4ce06870af339e90e888327e7a88689d3cdc
Nacho Coll <ignacio.coll@gmail.com>
2019-04-26 10:15:24 +0200

    VPC lambda parameters.

--- src/LambdaResourceProperties.cs
+++ src/LambdaResourceProperties.cs
@@ -3,9 +3,27 @@ using System;
 namespace Cloudformation4dotNET.Lambda
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class LambdaResourceProperties : Attribute {
+    public class LambdaResourceProperties : Attribute
+    {
 
-        public int TimeoutInSeconds { get; set;}  = 20;
-        
+        public int TimeoutInSeconds { get; set; } = 20;
+
+        /// <summary>
+        /// VPCSecurityGroupIdsRef and VPCSubnetIdsRef are required if you want your Lambda to access VPC.
+        /// Setting those values will add:
+        /// VpcConfig:
+        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName
+        ///     SubnetIds: !Ref VPCSubnetIdsParameterName
+        /// </summary>
+        public string VPCSecurityGroupIdsParameterName { get; set; } = "";
+
+        /// <summary>
+        /// VPCSecurityGroupIdsRef and VPCSubnetIdsRef are required if you want your Lambda to access VPC.
+        /// Setting those values will add:
+        /// VpcConfig:
+        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName
+        ///     SubnetIds: !Ref VPCSubnetIdsParameterName
+        /// </summary>
+        public string VPCSubnetIdsParameterName { get; set; } = "";
     }
 }
