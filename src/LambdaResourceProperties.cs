a62452658f0d3b6788a499f6b279399c5a090533
Nacho Coll <ignacio.coll@gmail.com>
2019-05-13 14:52:01 +0200

    Lambda VPC single Subnet/SG

--- src/LambdaResourceProperties.cs
+++ src/LambdaResourceProperties.cs
@@ -14,18 +14,30 @@ namespace Cloudformation4dotNET.Lambda
         /// VPCSecurityGroupIdsRef and VPCSubnetIdsRef are required if you want your Lambda to access VPC.
         /// Setting those values will add:
         /// VpcConfig:
-        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName
-        ///     SubnetIds: !Ref VPCSubnetIdsParameterName
+        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName <= List<AWS::EC2::SecurityGroup::Id>
+        ///     SubnetIds: !Ref VPCSubnetIdsParameterName <= List<AWS::EC2::Subnet::Id>
         /// </summary>
         public string VPCSecurityGroupIdsParameterName { get; set; } = "";
+
+        // In case you want to use a single Security Group.
+        // ...  SecurityGroupIds: 
+        //          - !Ref VPCSecurityGroup <= AWS::EC2::SecurityGroup::Id
+        public string VPCSecurityGroup { get; set; } = "";
 
         /// <summary>
         /// VPCSecurityGroupIdsRef and VPCSubnetIdsRef are required if you want your Lambda to access VPC.
         /// Setting those values will add:
         /// VpcConfig:
-        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName
-        ///     SubnetIds: !Ref VPCSubnetIdsParameterName
+        ///     SecurityGroupIds: !Ref VPCSecurityGroupIdsParameterName <= List<AWS::EC2::SecurityGroup::Id>
+        ///     SubnetIds: !Ref VPCSubnetIdsParameterName <= List<AWS::EC2::Subnet::Id>
         /// </summary>
         public string VPCSubnetIdsParameterName { get; set; } = "";
+
+        // In case you just want to use a single VPC Subnet.
+        // ...  SubnetIds:
+        //          - !Ref VPCSubnet <= AWS::EC2::Subnet::Id
+        public string VPCSubnet { get; set; } = "";
+
+
     }
 }
